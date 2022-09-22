69b793dd8660f785832db8e44a07561b879d89d7
Anjana Vakil <anjanavakil@gmail.com>
2022-09-21 21:44:27 -0500

    doggo modules

---
+++ 3-utilities.js
@@ -0,0 +1,27 @@
+function getRandomElement(array) {
+    const i = Math.floor(Math.random() * array.length);
+    return array[i];
+}
+
+function shuffleArray(array) {
+    // randomly shuffles elements in an array in-place
+    return array.sort((a,b) => Math.random() - 0.5);
+}
+
+function getChoices(n, correctAnswer, array) {
+    // Generate a list of n choices including the correct answer and others from the array
+    const choices = [correctAnswer];
+    while (choices.length < n) {
+        let candidate = getRandomElement(array);
+        if (choices.indexOf(candidate) < 0) { // check if this is already in the array
+            choices.push(candidate); // if not, add it
+        }
+    }
+    return shuffleArray(choices);
+}
+
+export {
+    getRandomElement,
+    shuffleArray,
+    getChoices
+};
